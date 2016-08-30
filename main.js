af477ddd95b0eeed7bbf4b592382f99b721f5922
militoj <militoj@gmail.com>
2016-08-29 21:30:49 -0500

    seago bubble population solution

--- main.js
+++ main.js
@@ -4,5 +4,17 @@ $( document ).ready(function() {
     alert("seago suck my balls")
   });
 
-  
+  var button = '#button';
+  var textfield = '#textfield';
+  var bubble = '#bubble';
+
+  var update_bubble = function(e){
+    e.preventDefault();
+    $(bubble).html(
+      $(textfield).val();
+    );
+  }
+
+  $(button).click(update_bubble);
+
 });
